dbbb42eda4a3f63e1c12a80816924b528e0a243b
Erin Reilly <erinmariereilly@gmail.com>
2019-02-18 14:51:31 -0500

    Added palates to the end

--- 03_custom-theme-for-plots.R
+++ 03_custom-theme-for-plots.R
@@ -17,3 +17,13 @@ theme_hoboplot<-function(){
         legend.key = element_rect(fill = NA), 
         legend.background = element_rect(fill = NA))
 }
+
+
+hpal1<-c("north"="#54086B", "mid" = "#FF0BAC", "south"="#00BEC5", "weir"="#050833")
+hpal2<-c("north"="#FF5C4D", "mid" = "#FF9636", "south"="#FFCD58", "weir"="#DAD870")
+hpal3<-c("north"="#FF8370", "mid" = "#00B1B0", "south"="#FEC84D", "weir"="#E42256")
+hpal4<-c("north"="#0A4158", "mid" = "#4B8378", "south"="#FF9636", "weir"="#E4D7D0")
+hpal5<-c("north"="#FFCE13", "mid" = "#88070B", "south"="#F65156", "weir"="#009292")
+hpal6<-c("north"="#F6A21E", "mid" = "#E55B13", "south"="#104210", "weir"="#7A871E")
+hpal7<-c("north"="#FF5765", "mid" = "#FFDB15", "south"="#8A6FDF", "weir"="#A8E10C")
+hpal8<-c("north"="#E69F00", "mid" = "#F0E442", "south"="#009E73", "weir"="#56B4E9")
